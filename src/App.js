de1763dd7e1a6a18728ba7ac984ac4c323bf4f90
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-25 11:27:19 +0600

    create_theme_context_and_make two child

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./component/Navbar";
 import Todolist from "./component/Todolist";
+import ThemeContextProvider from "./Context/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
+        <ThemeContextProvider>
      <Navbar />
      <Todolist />
+     </ThemeContextProvider>
      </div>
     </div>
   );
---
+++ src/Context/ThemeContext.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { createContext } from "react";
+export const ThemeContext=createContext();
+class ThemeContextProvider extends React.Component
+{
+    state={
+        isDarkTheme:true,
+        lightTheme:{
+            Text:'#222',
+            background:'#d8ddf1'
+
+        },
+        darkTheme:  {
+            Text:"#fff",
+            background:"#5c5c5c"
+        }
+
+    }
+    render()
+    {
+        return(
+            <ThemeContext.Provider value={{...this.state}}>
+                {this.props.children}
+
+            </ThemeContext.Provider>
+
+        );
+    }
+}
+export default ThemeContextProvider
