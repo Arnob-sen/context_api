29dfe05da84bc680225a509987cc807fda25258e
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-26 00:31:20 +0600

    create_multiple_context_with_useContext

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Navbar from "./component/Navbar";
 import Todolist from "./component/Todolist";
 import ThemeContextProvider from "./Context/ThemeContext";
+import AuthContextProvider from "./Context/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
+        <AuthContextProvider>
         <ThemeContextProvider>
      <Navbar />
      <Todolist />
      </ThemeContextProvider>
+     </AuthContextProvider>
      </div>
     </div>
   );
---
+++ src/Context/AuthContext.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { createContext } from "react";
+export const AuthContext=createContext();
+class AuthContextProvider extends React.Component
+{
+    state={
+        isLoggedIn:false,
+    };
+    changeAuthStatus=()=>{
+        this.setState({isLoggedIn:!this.state.isLoggedIn})
+    }
+    render()
+    {
+        return(
+            <AuthContext.Provider value={{...this.state,changeAuthStatus:this.changeAuthStatus}}>
+                {this.props.children}
+            </AuthContext.Provider>
+        )
+
+    }
+}
+export default AuthContextProvider
