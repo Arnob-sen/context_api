ba7427b0041bc8c930eef2983d72456e48d80c46
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-25 21:38:16 +0600

    add_theme_in_todolist

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,9 +7,10 @@ class Navbar extends React.Component
         const {isDarkTheme,darkTheme,lightTheme}=this.context;
         const Theme=isDarkTheme ? darkTheme : lightTheme;
         return(
-            <nav style={{background:Theme.background,color:Theme.Text,height:'120px'}}>
+            <nav style={{background:Theme.background,color:Theme.Text,height:'140px'}}>
                 <h2 style={{textAlign:'center'}}>my academy</h2>
-                <div className="ui three button" style={{textAlign:'center'}}>
+                <div className="ui five button" >
+                    <div style={{textAlign:'center'}}>
                     <button className="ui button">
                         overview
                     </button>
@@ -19,6 +20,17 @@ class Navbar extends React.Component
                     <button className="ui button">
                         contact
                     </button>
+                    <button className="ui button">
+                        dolist1
+                    </button>
+                    <button className="ui button">
+                        dolist2
+                    </button>
+                    <button className="ui button">
+                        dolist3
+                    </button>
+                    </div>
+
 
                 </div>
             </nav>
--- src/component/Todolist.js
+++ src/component/Todolist.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { ThemeContext } from "../Context/ThemeContext";
 class Todolist extends React.Component
 {
+    static contextType=ThemeContext;
+    
+
     render()
     {
+        const {isDarkTheme,darkTheme,lightTheme}=this.context;
+        const Theme=isDarkTheme ? darkTheme :lightTheme;
+        
         return(
-            <div className="ui list">
+            <div style={{background:Theme.background,color:Theme.Text,height:'140px',textAlign:'center'}}>
                 <p class="item">plan the family trip</p>
                 <p class="item">go for shopping for dinner</p>
                 <p class="item"> go for a walk</p>
