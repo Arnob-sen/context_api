9c613b238db3539157558653df95e37e3442400e
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-25 22:56:58 +0600

    another_wayto access context_using_contextconsumer

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,38 +2,48 @@ import React from "react";
 import { ThemeContext } from "../Context/ThemeContext";
 class Navbar extends React.Component
 {
-    static contextType=ThemeContext;
+  
     render(){
-        const {isDarkTheme,darkTheme,lightTheme}=this.context;
-        const Theme=isDarkTheme ? darkTheme : lightTheme;
+        
         return(
-            <nav style={{background:Theme.background,color:Theme.Text,height:'140px'}}>
-                <h2 style={{textAlign:'center'}}>my academy</h2>
-                <div className="ui five button" >
-                    <div style={{textAlign:'center'}}>
-                    <button className="ui button">
-                        overview
-                    </button>
-                    <button className="ui button">
-                        support
-                    </button>
-                    <button className="ui button">
-                        contact
-                    </button>
-                    <button className="ui button">
-                        dolist1
-                    </button>
-                    <button className="ui button">
-                        dolist2
-                    </button>
-                    <button className="ui button">
-                        dolist3
-                    </button>
-                    </div>
-
-
+          <ThemeContext.Consumer>
+            {(value)=>{
+                const {isDarkTheme,darkTheme,lightTheme}=value;
+                const Theme=isDarkTheme ? darkTheme : lightTheme;
+                return(
+                 <nav style={{background:Theme.background,color:Theme.Text,height:'140px'}}>
+              <h2 style={{textAlign:'center'}}>my academy</h2>
+              <div className="ui five button" >
+                 <div style={{textAlign:'center'}}>
+                <button className="ui button">
+                    overview
+                </button>
+                <button className="ui button">
+                    support
+                </button>
+                <button className="ui button">
+                    contact
+                </button>
+                <button className="ui button">
+                    dolist1
+                </button>
+                <button className="ui button">
+                    dolist2
+                </button>
+                <button className="ui button">
+                    dolist3
+                </button>
                 </div>
-            </nav>
+
+
+            </div>
+        </nav>
+
+                )
+            }}
+
+            </ThemeContext.Consumer>
+            
         )
     }
 }
