88898628cc88cf8d2a67c00fb219cac7d75e210b
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-25 21:10:19 +0600

    added_theme

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { ThemeContext } from "../Context/ThemeContext";
 class Navbar extends React.Component
 {
+    static contextType=ThemeContext;
     render(){
+        const {isDarkTheme,darkTheme,lightTheme}=this.context;
+        const Theme=isDarkTheme ? darkTheme : lightTheme;
         return(
-            <nav>
-                <h2 className="ui centered header">my academy</h2>
-                <div className="ui three button">
+            <nav style={{background:Theme.background,color:Theme.Text,height:'120px'}}>
+                <h2 style={{textAlign:'center'}}>my academy</h2>
+                <div className="ui three button" style={{textAlign:'center'}}>
                     <button className="ui button">
                         overview
                     </button>
